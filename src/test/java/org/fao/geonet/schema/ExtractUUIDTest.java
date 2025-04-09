5124a9f559933d70ae0ea610d418ae50f9eac1f2
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-04-09 17:10:52 +0200

    adapt extract-uuid.xsl

---
+++ src/test/java/org/fao/geonet/schema/ExtractUUIDTest.java
@@ -0,0 +1,30 @@
+package org.fao.geonet.schema;
+
+import org.fao.geonet.schema.iso19115_3_2018_che.ISO19115_3_2018SchemaPlugin;
+import org.fao.geonet.schemas.XslProcessTest;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Element;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExtractUUIDTest extends XslProcessTest {
+
+    public ExtractUUIDTest() {
+        super();
+        this.setNs(ISO19115_3_2018SchemaPlugin.allNamespaces);
+    }
+
+    @Test
+    public void extractUUID() throws Exception {
+        xslFile = Paths.get(testClass.getClassLoader().getResource("extract-uuid.xsl").toURI());
+        xmlFile = Paths.get(testClass.getClassLoader().getResource("amphibians-19115-3.che.xml").toURI());
+        Element amphibians = Xml.loadFile(xmlFile);
+
+        Element extractedUUID = Xml.transform(amphibians, xslFile);
+
+        assertEquals("9ea54bf1-43b5-4cbd-ab46-0a9dd65567ce", extractedUUID.getText());
+    }
+}
