d348a45dfcf261cde2712657ed2401ab50becc55
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-04-17 12:11:33 +0200

    remove 'extends XslProcessTest'

--- src/test/java/org/fao/geonet/schema/XslConversionTest.java
+++ src/test/java/org/fao/geonet/schema/XslConversionTest.java
@@ -30,8 +30,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-import org.fao.geonet.schema.iso19115_3_2018_che.ISO19115_3_2018SchemaPlugin;
-import org.fao.geonet.schemas.XslProcessTest;
+import org.fao.geonet.utils.TransformerFactoryFactory;
 import org.fao.geonet.utils.Xml;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -44,6 +43,7 @@ import org.jdom.Namespace;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.jdom.xpath.XPath;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 import org.xmlunit.builder.DiffBuilder;
@@ -58,18 +58,18 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-public class XslConversionTest extends XslProcessTest {
+public class XslConversionTest {
 
-    public XslConversionTest() {
-        super();
-        this.setNs(ISO19115_3_2018SchemaPlugin.allNamespaces);
+    @BeforeClass
+    public static void initSaxon() {
+        TransformerFactoryFactory.init("net.sf.saxon.TransformerFactoryImpl");
     }
 
     @Test
     public void testOdsConversion() throws Exception {
-        xslFile = Paths.get(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromJsonOpenDataSoft.xsl").toURI());
-        xmlFile = Paths.get(testClass.getClassLoader().getResource("ods.xml").toURI());
-        Path jsonFile = Paths.get(testClass.getClassLoader().getResource("ods.json").toURI());
+        Path xslFile = Paths.get(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromJsonOpenDataSoft.xsl").toURI());
+        Path xmlFile = Paths.get(getClass().getClassLoader().getResource("ods.xml").toURI());
+        Path jsonFile = Paths.get(getClass().getClassLoader().getResource("ods.json").toURI());
         String jsonString = Files.readString(jsonFile);
         Element xmlFromJSON = Xml.getXmlFromJSON(jsonString);
         xmlFromJSON.setName("record");
@@ -96,8 +96,8 @@ public class XslConversionTest extends XslProcessTest {
 
     @Test
     public void validateAmphibiansSchema() throws Exception {
-        xslFile = Paths.get(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromISO19139.xsl").toURI());
-        xmlFile = Paths.get(testClass.getClassLoader().getResource("amphibians-19139.che.xml").toURI());
+        Path xslFile = Paths.get(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromISO19139.xsl").toURI());
+        Path xmlFile = Paths.get(getClass().getClassLoader().getResource("amphibians-19139.che.xml").toURI());
         Element amphibians = Xml.loadFile(xmlFile);
 
         Element amphibiansIso19115che = Xml.transform(amphibians, xslFile);
@@ -105,7 +105,7 @@ public class XslConversionTest extends XslProcessTest {
         //TODO CMT/SRT activate
         //isGNValid(amphibiansIso19115che);
 
-        byte[] expected = testClass.getClassLoader().getResourceAsStream("amphibians-19115-3.che.xml").readAllBytes();
+        byte[] expected = getClass().getClassLoader().getResourceAsStream("amphibians-19115-3.che.xml").readAllBytes();
         byte[] actual = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n")) //
                 .outputString(new Document(amphibiansIso19115che)) //
                 .getBytes(StandardCharsets.UTF_8);
@@ -115,7 +115,7 @@ public class XslConversionTest extends XslProcessTest {
 
     private void isValid(Element xmlIso19115che) throws SAXException, IOException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Source schemaFile = new StreamSource(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/schema/standards.iso.org/19115/-3/eCH-0271-1-0-0.xsd").getFile());
+        Source schemaFile = new StreamSource(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/schema/standards.iso.org/19115/-3/eCH-0271-1-0-0.xsd").getFile());
 
         Schema schema = factory.newSchema(schemaFile);
         Validator validator = schema.newValidator();
@@ -131,26 +131,26 @@ public class XslConversionTest extends XslProcessTest {
     }
 
     private void isGNValid(Element amphibiansIso19115che) throws Exception {
-        Xml.validate(Path.of(testClass.getClassLoader().getResource("schema/standards.iso.org/19115/-3/eCH-0271-1-0-0.xsd").toURI()), amphibiansIso19115che);
+        Xml.validate(Path.of(getClass().getClassLoader().getResource("schema/standards.iso.org/19115/-3/eCH-0271-1-0-0.xsd").toURI()), amphibiansIso19115che);
     }
 
     @Test
     public void convertResponsibleParty() throws Exception {
-        xslFile = Paths.get(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/ISO19139/mapping/CI_ResponsibleParty.xsl").toURI());
-        xmlFile = Paths.get(testClass.getClassLoader().getResource("responsible_party_agroscope_iso19139_che.xml").toURI());
+        Path xslFile = Paths.get(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/ISO19139/mapping/CI_ResponsibleParty.xsl").toURI());
+        Path xmlFile = Paths.get(getClass().getClassLoader().getResource("responsible_party_agroscope_iso19139_che.xml").toURI());
         Element amphibians = Xml.loadFile(xmlFile);
 
         Element newRespParty = Xml.transform(amphibians, xslFile);
 
-        byte[] expected = testClass.getClassLoader().getResourceAsStream("expectedFromNewRespParty.xml").readAllBytes();
+        byte[] expected = getClass().getClassLoader().getResourceAsStream("expectedFromNewRespParty.xml").readAllBytes();
         byte[] actual = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n")).outputString(newRespParty).getBytes(StandardCharsets.UTF_8);
         assertArrayEquals(expected, actual);
     }
 
     @Test
     public void validateGruenflaechenSchema() throws Exception {
-        xslFile = Paths.get(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromISO19139.xsl").toURI());
-        xmlFile = Paths.get(testClass.getClassLoader().getResource("gruenflaechen-19139.che.xml").toURI());
+        Path xslFile = Paths.get(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/convert/fromISO19139.xsl").toURI());
+        Path xmlFile = Paths.get(getClass().getClassLoader().getResource("gruenflaechen-19139.che.xml").toURI());
         Element gruenflaechen = Xml.loadFile(xmlFile);
 
         Element gruenflaechenIso19115che = Xml.transform(gruenflaechen, xslFile);
