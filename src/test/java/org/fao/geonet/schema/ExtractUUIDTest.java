af7a466d34794c8db383b0ec1ecf1bb1f1f7a645
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-04-17 12:11:33 +0200

    remove 'extends XslProcessTest'

--- src/test/java/org/fao/geonet/schema/ExtractUUIDTest.java
+++ src/test/java/org/fao/geonet/schema/ExtractUUIDTest.java
@@ -1,26 +1,20 @@
 package org.fao.geonet.schema;
 
-import org.fao.geonet.schema.iso19115_3_2018_che.ISO19115_3_2018SchemaPlugin;
-import org.fao.geonet.schemas.XslProcessTest;
 import org.fao.geonet.utils.Xml;
 import org.jdom.Element;
 import org.junit.Test;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.Assert.assertEquals;
 
-public class ExtractUUIDTest extends XslProcessTest {
-
-    public ExtractUUIDTest() {
-        super();
-        this.setNs(ISO19115_3_2018SchemaPlugin.allNamespaces);
-    }
+public class ExtractUUIDTest {
 
     @Test
     public void extractUUID() throws Exception {
-        xslFile = Paths.get(testClass.getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/extract-uuid.xsl").toURI());
-        xmlFile = Paths.get(testClass.getClassLoader().getResource("amphibians-19115-3.che.xml").toURI());
+        Path xslFile = Paths.get(getClass().getClassLoader().getResource("gn-site/WEB-INF/data/config/schema_plugins/iso19115-3.2018.che/extract-uuid.xsl").toURI());
+        Path xmlFile = Paths.get(getClass().getClassLoader().getResource("amphibians-19115-3.che.xml").toURI());
         Element amphibians = Xml.loadFile(xmlFile);
 
         Element extractedUUID = Xml.transform(amphibians, xslFile);
