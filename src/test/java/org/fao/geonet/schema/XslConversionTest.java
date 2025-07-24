76d08939e582868523dec7dbaf01c85877abcabe
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-07-24 16:53:24 +0200

    add missing xlink process/role attr conversion

--- src/test/java/org/fao/geonet/schema/XslConversionTest.java
+++ src/test/java/org/fao/geonet/schema/XslConversionTest.java
@@ -185,6 +185,11 @@ public class XslConversionTest {
     }
 
     @Test
+    public void convertRawZonesDeTranquillite() throws Exception {
+        transformAndCompare("raw-zonesDeTranquillite", false);
+    }
+
+    @Test
     public void testOdsConversion() throws Exception {
         Element xmlFromJSON = Xml.getXmlFromJSON(Files.readString(getResource("ods.json")));
         xmlFromJSON.setName("record");
