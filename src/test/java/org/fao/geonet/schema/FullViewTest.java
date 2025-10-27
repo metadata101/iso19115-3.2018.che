cb8fa94dcf47687c9027d5cd6ecfed8d56f90d2e
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-10-27 15:13:46 +0100

    full view: legislationInfo tab and avoid onlineResource linkage error

--- src/test/java/org/fao/geonet/schema/FullViewTest.java
+++ src/test/java/org/fao/geonet/schema/FullViewTest.java
@@ -31,8 +31,8 @@ import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
@@ -42,7 +42,6 @@ import java.util.Map;
 
 import static org.fao.geonet.schema.TestSupport.getResource;
 import static org.fao.geonet.schema.TestSupport.getResourceInsideSchema;
-import static org.jdom.Namespace.getNamespace;
 
 public class FullViewTest {
 
@@ -50,13 +49,13 @@ public class FullViewTest {
 
     private static Field resolverMapField;
 
-    @Before // To reset saxon before each test will ensure xsl generate-id consistency
-    public void initSaxon() {
+    @BeforeClass
+    public static void initSaxon() {
         TransformerFactoryFactory.init("net.sf.saxon.TransformerFactoryImpl");
     }
 
-    @Before
-    public void initOasis() throws NoSuchFieldException, IllegalAccessException, URISyntaxException {
+    @BeforeClass
+    public static void initOasis() throws NoSuchFieldException, IllegalAccessException, URISyntaxException {
         resolverMapField = ResolverWrapper.class.getDeclaredField("resolverMap");
         resolverMapField.setAccessible(true);
         ((Map<?, ?>) resolverMapField.get(null)).clear();
@@ -67,43 +66,27 @@ public class FullViewTest {
         ResolverWrapper.getInstance().setBlankXSLFile(getResource("config/blank.xsl").toAbsolutePath().toString());
         NioPathAwareCatalogResolver.addRewriteDirective(
                     new PrefixUrlRewrite("sharedFormatterDir/",
-                            getResource("gn-site/WEB-INF/data/data/formatter").toAbsolutePath() + "/"));
+                    getResource("gn-site/WEB-INF/data/data/formatter").toAbsolutePath() + "/"));
     }
 
-    @After
-    public void clearOasis() throws IllegalAccessException {
+    @AfterClass
+    public static void clearOasis() throws IllegalAccessException {
         ((Map<?,?>) resolverMapField.get(null)).clear();
     }
 
 
     @Test
-    public void fullViewMultilingual() throws Exception {
+    public void asiatischeHornisse() throws Exception {
         Path xslFile = getResourceInsideSchema("formatter/xsl-view/view.xsl");
-        Path xmlFile = getResource("UpperRhineCastles-ISO19115-3-full-view-form.xml");
+        Path xmlFile = getResource("asiatischeHornisse-19115-3.che-full-view-form.xml");
         Element source = Xml.loadFile(xmlFile);
 
         Element transformed = Xml.transform(source, xslFile, Map.of("view", "advanced", "approved", "true", "root", "div"));
 
-        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
-        String actual = xmlOutputter.outputString(new Document(transformed));
-        TestSupport.assertGeneratedDataByteMatchExpected("UpperRhineCastles-ISO19115-3-full-view.html", actual, GENERATE_EXPECTED_FILE);
-    }
-
-    @Test
-    public void fullViewTwoLineagesAndConstraints() throws Exception {
-        Path xslFile = getResourceInsideSchema("formatter/xsl-view/view.xsl");
-        Element form = Xml.loadFile(getResource("UpperRhineCastles-ISO19115-3-full-view-form.xml"));
-        Element md = Xml.loadFile(getResource("two-lineages-and-constraints-iso19115-3.2018.xml"));
-        Element metadataToReplace = form.getChild("MD_Metadata", getNamespace("mdb", "http://standards.iso.org/iso/19115/-3/mdb/2.0"));
-        Element parent = metadataToReplace.getParentElement();
-        int index = parent.indexOf(metadataToReplace);
-        parent.setContent(index, md.detach());
-
-        Element transformed = Xml.transform(form, xslFile, Map.of("view", "advanced", "approved", "true", "root", "div"));
 
         XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
         String actual = xmlOutputter.outputString(new Document(transformed));
-        TestSupport.assertGeneratedDataByteMatchExpected("two-lineages-and-constraints-ISO19115-3-full-view.html", actual, GENERATE_EXPECTED_FILE);
+        TestSupport.assertGeneratedDataByteMatchExpected("asiatischeHornisse-19115-3.che-full-view.html", actual, GENERATE_EXPECTED_FILE);
     }
 
     private static Path addRequiredSchemasAndDisableConflictingOne() throws URISyntaxException {
