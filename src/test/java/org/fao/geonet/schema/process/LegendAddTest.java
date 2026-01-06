9eda3060244fea07def68f156b904e14e5a80c90
christophe mangeat <christophe.mangeat@camptocamp.com>
2026-01-06 16:14:16 +0100

    Specialise add legend process

---
+++ src/test/java/org/fao/geonet/schema/process/LegendAddTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2001-2024 Food and Agriculture Organization of the
+ * United Nations (FAO-UN), United Nations World Food Programme (WFP)
+ * and United Nations Environment Programme (UNEP)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Contact: Jeroen Ticheler - FAO - Viale delle Terme di Caracalla 2,
+ * Rome - Italy. email: geonetwork@osgeo.org
+ */
+package org.fao.geonet.schema.process;
+
+import org.fao.geonet.schema.TestSupport;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+
+import static org.fao.geonet.schema.TestSupport.getResource;
+import static org.fao.geonet.schema.TestSupport.getResourceInsideSchema;
+
+public class LegendAddTest extends AbstractProcessTest {
+
+    @Test
+    public void asiatischeHornisse() throws Exception {
+        Path xslFile = getResourceInsideSchema("process/legend-add.xsl");
+        Path xmlFile = getResource("asiatischeHornisse-19115-3.che.xml");
+        Element source = Xml.loadFile(xmlFile);
+
+        Element transformed = Xml.transform(source, xslFile, new HashMap<>(){
+            {
+                put("process", "legend-add");
+                put("url", "DE#http://localhost:8080/geonetwork/srv/api/records/786606bc-2b81-4426-8ccd-e3a2c76cdb14/attachments/corfu.png|FR#http://localhost:8080/geonetwork/srv/api/records/786606bc-2b81-4426-8ccd-e3a2c76cdb14/attachments/corfu.png|IT#http://localhost:8080/geonetwork/srv/api/records/786606bc-2b81-4426-8ccd-e3a2c76cdb14/attachments/corfu.png|EN#http://localhost:8080/geonetwork/srv/api/records/786606bc-2b81-4426-8ccd-e3a2c76cdb14/attachments/corfu.png");
+                put("protocol", "WWW:LINK");
+                put("function", "legend");
+                put("name", "DE#Légende pour ArcGIS (LYR)|FR#Légende pour ArcGIS (LYR)|IT#Légende pour ArcGIS (LYR)|EN#Légende pour ArcGIS (LYR)");
+            }});
+
+        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
+        String actual = xmlOutputter.outputString(new Document(transformed));
+        TestSupport.assertGeneratedDataByteMatchExpected("processes/asiatischeHornisse-19115-3.che-with-added-legend.xml", actual, GENERATE_EXPECTED_FILE);
+    }
+
+}
