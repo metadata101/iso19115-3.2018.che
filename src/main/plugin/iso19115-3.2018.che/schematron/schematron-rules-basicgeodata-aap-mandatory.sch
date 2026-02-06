ae2fd21bbe9cc0712f11ac23c67432b9393fedb7
christophe mangeat <christophe.mangeat@camptocamp.com>
2026-02-06 11:24:05 +0100

    schematron basicgeodata-aap: 'basicGeodata = ‘true’ implies basicGeodataInformation is mandatory'

---
+++ src/main/plugin/iso19115-3.2018.che/schematron/schematron-rules-basicgeodata-aap-mandatory.sch
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<sch:schema xmlns:sch="http://purl.oclc.org/dsdl/schematron">
+  <sch:title xmlns="http://www.w3.org/2001/XMLSchema" xml:lang="en">Basic Geodata - AAP mandatory rules</sch:title>
+  <sch:title xmlns="http://www.w3.org/2001/XMLSchema" xml:lang="fr">Basic Geodata - AAP règles obligatoires</sch:title>
+  <sch:ns prefix="gml" uri="http://www.opengis.net/gml/3.2"/>
+  <sch:ns prefix="srv" uri="http://standards.iso.org/iso/19115/-3/srv/2.0"/>
+  <sch:ns prefix="cit" uri="http://standards.iso.org/iso/19115/-3/cit/2.0"/>
+  <sch:ns prefix="gex" uri="http://standards.iso.org/iso/19115/-3/gex/1.0"/>
+  <sch:ns prefix="mco" uri="http://standards.iso.org/iso/19115/-3/mco/1.0"/>
+  <sch:ns prefix="mdb" uri="http://standards.iso.org/iso/19115/-3/mdb/2.0"/>
+  <sch:ns prefix="mex" uri="http://standards.iso.org/iso/19115/-3/mex/1.0"/>
+  <sch:ns prefix="mmi" uri="http://standards.iso.org/iso/19115/-3/mmi/1.0"/>
+  <sch:ns prefix="gmw" uri="http://standards.iso.org/iso/19115/-3/gmw/1.0"/>
+  <sch:ns prefix="mrc" uri="http://standards.iso.org/iso/19115/-3/mrc/2.0"/>
+  <sch:ns prefix="mrd" uri="http://standards.iso.org/iso/19115/-3/mrd/1.0"/>
+  <sch:ns prefix="mri" uri="http://standards.iso.org/iso/19115/-3/mri/1.0"/>
+  <sch:ns prefix="mrs" uri="http://standards.iso.org/iso/19115/-3/mrs/1.0"/>
+  <sch:ns prefix="mcc" uri="http://standards.iso.org/iso/19115/-3/mcc/1.0"/>
+  <sch:ns prefix="lan" uri="http://standards.iso.org/iso/19115/-3/lan/1.0"/>
+  <sch:ns prefix="gco" uri="http://standards.iso.org/iso/19115/-3/gco/1.0"/>
+  <sch:ns prefix="mrl" uri="http://standards.iso.org/iso/19115/-3/mrl/2.0"/>
+  <sch:ns prefix="geonet" uri="http://www.fao.org/geonetwork"/>
+  <sch:ns prefix="xlink" uri="http://www.w3.org/1999/xlink"/>
+  <sch:ns prefix="xsi" uri="http://www.w3.org/2001/XMLSchema-instance"/>
+  <sch:ns prefix="che" uri="http://geocat.ch/che"/>
+
+  <sch:diagnostics>
+    <sch:diagnostic id="rule.basicgeodata.basicgeodatainformation-mandatory-failure-en" xml:lang="en">
+      When basicGeodata is 'true', basicGeodataInformation must be present.
+    </sch:diagnostic>
+    <sch:diagnostic id="rule.basicgeodata.basicgeodatainformation-mandatory-failure-fr" xml:lang="fr">
+      Lorsque basicGeodata vaut 'true', basicGeodataInformation doit être présent.
+    </sch:diagnostic>
+    <sch:diagnostic id="rule.basicgeodata.basicgeodatainformation-mandatory-success-en" xml:lang="en">
+      basicGeodataInformation is present when basicGeodata is 'true'.
+    </sch:diagnostic>
+    <sch:diagnostic id="rule.basicgeodata.basicgeodatainformation-mandatory-success-fr" xml:lang="fr">
+      basicGeodataInformation est présent lorsque basicGeodata vaut 'true'.
+    </sch:diagnostic>
+  </sch:diagnostics>
+
+  <sch:pattern id="rule.basicgeodata.basicgeodatainformation-mandatory">
+    <sch:title xml:lang="en">{basicGeodata = ‘true’ implies basicGeodataInformation is mandatory}</sch:title>
+    <sch:title xml:lang="fr">{basicGeodata = ‘true’ implique que basicGeodataInformation est obligatoire}</sch:title>
+
+    <sch:rule context="//che:CHE_MD_Metadata/mdb:identificationInfo/che:CHE_MD_DataIdentification[che:basicGeodata/gco:Boolean = 'true']">
+      <sch:assert test="che:basicGeodataInformation"
+                  diagnostics="rule.basicgeodata.basicgeodatainformation-mandatory-failure-en rule.basicgeodata.basicgeodatainformation-mandatory-failure-fr"/>
+      <sch:report test="che:basicGeodataInformation"
+                 diagnostics="rule.basicgeodata.basicgeodatainformation-mandatory-success-en rule.basicgeodata.basicgeodatainformation-mandatory-success-fr"/>
+    </sch:rule>
+  </sch:pattern>
+</sch:schema>
---
+++ src/test/java/org/fao/geonet/schema/schematron/SchematronBasicGeodataAapMandatoryTest.java
@@ -0,0 +1,109 @@
+package org.fao.geonet.schema.schematron;
+
+import org.fao.geonet.schema.TestSupport;
+import org.fao.geonet.utils.IO;
+import org.fao.geonet.utils.TransformerFactoryFactory;
+import org.fao.geonet.utils.Xml;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static org.fao.geonet.schema.TestSupport.getResource;
+import static org.fao.geonet.schema.TestSupport.getResourceInsideSchema;
+import static org.junit.Assert.assertEquals;
+
+public class SchematronBasicGeodataAapMandatoryTest {
+
+	private static final boolean GENERATE_EXPECTED_FILE = false;
+
+	private static Path compiledSchematronFilePath;
+
+	@ClassRule
+	public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+	public static void mustInitSaxonFirst() {
+		TransformerFactoryFactory.init("net.sf.saxon.TransformerFactoryImpl");
+	}
+
+	@BeforeClass
+	public static void initSaxonAndCompileSchematron() throws Exception {
+		mustInitSaxonFirst();
+		Element schematronSource = Xml.loadFile(getResourceInsideSchema("schematron/schematron-rules-basicgeodata-aap-mandatory.sch"));
+		Path schematronCompilation = getResource("gn-site/WEB-INF/classes/schematron/iso_svrl_for_xslt2.xsl");
+		Element compiledSchematron = Xml.transform(schematronSource, schematronCompilation);
+		compiledSchematronFilePath = temporaryFolder.getRoot().toPath().resolve("path/requiredtoFind/utilsfile/compiled-iso-schematron.xsl");
+		Files.createDirectories(compiledSchematronFilePath.getParent());
+		Files.write(compiledSchematronFilePath, Xml.getString(compiledSchematron).getBytes(StandardCharsets.UTF_8));
+	}
+
+	@BeforeClass
+	public static void makeUtilsFnAvailable() throws IOException, URISyntaxException {
+		Path targetUtilsFnFile = temporaryFolder.getRoot().toPath().resolve("xsl/utils-fn.xsl");
+		Files.createDirectories(targetUtilsFnFile.getParent());
+		IO.copyDirectoryOrFile( getResource("gn-site/xsl/utils-fn.xsl"), targetUtilsFnFile, false);
+	}
+
+	@Test
+	public void amphibiansIsoSchematron() throws Exception {
+		String report = applySchematronAndCompare("amphibians", false);
+
+        hasExpectedNumberOfFailure(0, report);
+	}
+
+	@Test
+	public void amphibiansIsoSchematronNoGeodataInformation() throws Exception {
+		Path xmlFile = getResource("amphibians" + "-19115-3.che.xml");
+		Element md = Xml.loadFile(xmlFile);
+		Xml.selectElement(md, "*//che:basicGeodataInformation").detach();
+
+		String report = applySchematronAndCompare("amphibians-no-geodataInfo", false, md);
+
+		hasExpectedNumberOfFailure(1, report);
+	}
+
+	private String applySchematronAndCompare(String mdNameRoot, boolean forceCreationDate) throws Exception {
+		Path xmlFile = getResource(mdNameRoot + "-19115-3.che.xml");
+		Element md = Xml.loadFile(xmlFile);
+		return applySchematronAndCompare(mdNameRoot, forceCreationDate, md);
+	}
+
+	private static String applySchematronAndCompare(String mdNameRoot, boolean forceCreationDate, Element md) throws Exception {
+		if (forceCreationDate) {
+			Element revisionDate = Xml.selectElement(md, "mdb:dateInfo");
+			Element creationDate = (Element) revisionDate.clone();
+			Element creationDateType = Xml.selectElement(creationDate, "cit:CI_Date/cit:dateType/cit:CI_DateTypeCode", List.of(Namespace.getNamespace("cit", "http://standards.iso.org/iso/19115/-3/cit/2.0")));
+			creationDateType.setText("creation");
+			creationDateType.getAttribute("codeListValue").setValue("creation");
+			md.addContent(md.indexOf(revisionDate) + 1, creationDate);
+		}
+		Element report = Xml.transform(md, compiledSchematronFilePath, Map.of(
+				"rule", "schematron-rules-iso",
+				"thesaurusDir", getResource("gn-site/WEB-INF/data/config/codelist").toAbsolutePath().toString(),
+				"lang", "fre"
+		));
+
+		XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
+		String actual = xmlOutputter.outputString(new Document(report));
+		TestSupport.assertGeneratedDataByteMatchExpected("schematron/" + mdNameRoot + "-schematron-rules-basicgeodata-aap-mandatory-report.xml", actual, GENERATE_EXPECTED_FILE);
+		return actual;
+	}
+
+    private static void hasExpectedNumberOfFailure(int expected, String report) {
+        assertEquals(expected, Pattern.compile("<svrl:failed-assert").matcher(report).results().count());
+    }
+}
