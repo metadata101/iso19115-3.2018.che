81d0ccec5beb0218925a260e6a85fb2ea0196643
sebr72 <sebastien_riollet@hotmail.com>
2026-02-17 16:35:49 +0100

    Refactor schematrons tests

--- src/test/java/org/fao/geonet/schema/schematron/SchematronIsoTest.java
+++ src/test/java/org/fao/geonet/schema/schematron/SchematronIsoTest.java
@@ -1,67 +1,34 @@
 package org.fao.geonet.schema.schematron;
 
-import org.fao.geonet.schema.TestSupport;
-import org.fao.geonet.utils.IO;
-import org.fao.geonet.utils.TransformerFactoryFactory;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.fao.geonet.utils.Xml;
-import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.Namespace;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 import static org.fao.geonet.schema.TestSupport.getResource;
-import static org.fao.geonet.schema.TestSupport.getResourceInsideSchema;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class SchematronIsoTest {
+public class SchematronIsoTest extends AbstractSchematronTest {
 
-	private static final boolean GENERATE_EXPECTED_FILE = false;
-
-	private static Path compiledSchematronFilePath;
-
-	@ClassRule
-	public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
-
-	public static void mustInitSaxonFirst() {
-		TransformerFactoryFactory.init("net.sf.saxon.TransformerFactoryImpl");
-	}
+	private static final String SCHEMATRON_NAME = "schematron-rules-iso";
 
 	@BeforeClass
 	public static void initSaxonAndCompileSchematron() throws Exception {
-		mustInitSaxonFirst();
-		Element schematronSource = Xml.loadFile(getResourceInsideSchema("schematron/schematron-rules-iso.sch"));
-		Path schematronCompilation = getResource("gn-site/WEB-INF/classes/schematron/iso_svrl_for_xslt2.xsl");
-		Element compiledSchematron = Xml.transform(schematronSource, schematronCompilation);
-		compiledSchematronFilePath = temporaryFolder.getRoot().toPath().resolve("path/requiredtoFind/utilsfile/compiled-iso-schematron.xsl");
-		Files.createDirectories(compiledSchematronFilePath.getParent());
-		Files.write(compiledSchematronFilePath, Xml.getString(compiledSchematron).getBytes(StandardCharsets.UTF_8));
+		initSaxonAndCompileSchematron("schematron/" + SCHEMATRON_NAME + ".sch");
 	}
 
-	@BeforeClass
-	public static void makeUtilsFnAvailable() throws IOException, URISyntaxException {
-		Path targetUtilsFnFile = temporaryFolder.getRoot().toPath().resolve("xsl/utils-fn.xsl");
-		Files.createDirectories(targetUtilsFnFile.getParent());
-		IO.copyDirectoryOrFile( getResource("gn-site/xsl/utils-fn.xsl"), targetUtilsFnFile, false);
+	protected @NonNull String getSchematronName() {
+		return SCHEMATRON_NAME;
 	}
 
 	@Test
 	public void amphibians() throws Exception {
-		String report = applySchematronAndCompare("amphibians", false);
+		String report = applySchematronAndCompare("amphibians");
 
         hasExpectedNumberOfFailure(2, report);
 	}
@@ -70,7 +37,7 @@ public class SchematronIsoTest {
 	public void amphibiansFailure() throws Exception {
 		Path xmlFile = getResource("schematron/amphibians-iso-schematron-failure-19115-3.che.xml");
 		Element md = Xml.loadFile(xmlFile);
-		String report = applySchematronAndCompare("amphibians-iso-schematron-failure", false, md);
+		String report = applySchematronAndCompare("amphibians-iso-schematron-failure", md);
 
         hasExpectedNumberOfFailure(14, report);
 	}
@@ -78,7 +45,7 @@ public class SchematronIsoTest {
 
 	@Test
 	public void amphibiansWithUpdatedFixedInfo() throws Exception {
-		String report = applySchematronAndCompare("amphibians-with-updated-fixed-info", false);
+		String report = applySchematronAndCompare("amphibians-with-updated-fixed-info");
 
 		hasExpectedNumberOfFailure(1, report);
 	}
@@ -114,36 +81,4 @@ public class SchematronIsoTest {
 
         hasExpectedNumberOfFailure(0, report);
 	}
-
-
-	private String applySchematronAndCompare(String mdNameRoot, boolean forceCreationDate) throws Exception {
-		Path xmlFile = getResource(mdNameRoot + "-19115-3.che.xml");
-		Element md = Xml.loadFile(xmlFile);
-		return applySchematronAndCompare(mdNameRoot, forceCreationDate, md);
-	}
-
-	private static String applySchematronAndCompare(String mdNameRoot, boolean forceCreationDate, Element md) throws Exception {
-		if (forceCreationDate) {
-			Element revisionDate = Xml.selectElement(md, "mdb:dateInfo");
-			Element creationDate = (Element) revisionDate.clone();
-			Element creationDateType = Xml.selectElement(creationDate, "cit:CI_Date/cit:dateType/cit:CI_DateTypeCode", List.of(Namespace.getNamespace("cit", "http://standards.iso.org/iso/19115/-3/cit/2.0")));
-			creationDateType.setText("creation");
-			creationDateType.getAttribute("codeListValue").setValue("creation");
-			md.addContent(md.indexOf(revisionDate) + 1, creationDate);
-		}
-		Element report = Xml.transform(md, compiledSchematronFilePath, Map.of(
-				"rule", "schematron-rules-iso",
-				"thesaurusDir", getResource("gn-site/WEB-INF/data/config/codelist").toAbsolutePath().toString(),
-				"lang", "fre"
-		));
-
-		XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat().setLineSeparator("\n"));
-		String actual = xmlOutputter.outputString(new Document(report));
-		TestSupport.assertGeneratedDataByteMatchExpected("schematron/" + mdNameRoot + "-schematron-rules-iso-report.xml", actual, GENERATE_EXPECTED_FILE);
-		return actual;
-	}
-
-    private static void hasExpectedNumberOfFailure(int expected, String report) {
-        assertEquals(expected, Pattern.compile("<svrl:failed-assert").matcher(report).results().count());
-    }
 }
