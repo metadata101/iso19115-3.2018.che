ead60eee5de658d6ec87d46a88188dc885740b8d
OCurdy <olivier_curdy@hotmail.com>
2026-02-23 13:34:08 +0100

    Add DCAT-AP-CH formatter and CSW outputSchema configuration

--- src/test/java/org/fao/geonet/schema/DCatFormatterTest.java
+++ src/test/java/org/fao/geonet/schema/DCatFormatterTest.java
@@ -66,6 +66,91 @@ public class DCatFormatterTest {
 		transformToDCatAndCompare("eu-dcat-ap", "grundwasservorkommen");
 	}
 
+	@Test
+	public void chDcatAp() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "grundwasservorkommen");
+	}
+
+	@Test
+	public void chDcatApAmphibians() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "amphibians");
+	}
+
+	@Test
+	public void chDcatApVeterinarians() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "veterinarians");
+	}
+
+	@Test
+	public void chDcatApConventionDesAlpes() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "conventionDesAlpesTousLesChamps");
+	}
+
+	@Test
+	public void chDcatApTrees() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "trees");
+	}
+
+	@Test
+	public void chDcatApGrundWasserSchutzZonen() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "grundwasserschutzzonen");
+	}
+
+	@Test
+	public void chDcatApAsiatischeHornisse() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "asiatischeHornisse");
+	}
+
+	@Test
+	public void chDcatApZonesDeTranquillite() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "zonesDeTranquillite");
+	}
+
+	@Test
+	public void chDcatApSwissTLM3D() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "swissTLM3D");
+	}
+
+	@Test
+	public void chDcatApModellDokumentationSwissTLM3D() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "modellDokumentationSwissTLM3D");
+	}
+
+	@Test
+	public void chDcatApVALTRALOC() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "VALTRALOC");
+	}
+
+	@Test
+	public void chDcatApOrganischenBodenInDerSchweiz() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "organischenBodenInDerSchweiz");
+	}
+
+	@Test
+	public void chDcatApSchuetzenswerte() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "schuetzenswerte");
+	}
+
+	@Test
+	public void chDcatApWeatherStations() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "weatherStations");
+	}
+
+	@Test
+	public void chDcatApHoheitsgrenzpunkteLV() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "hoheitsgrenzpunkteLV");
+	}
+
+	@Test
+	public void chDcatApFiktiverDarstellungskatalogMitURL() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "fiktiverDarstellungskatalogMitURL");
+	}
+
+	@Test
+	public void chDcatApWanderWege() throws Exception {
+		transformToDCatAndCompare("dcat-ap-ch", "wanderWege");
+	}
+
 	private void transformToDCatAndCompare(String profile, String mdNameRoot) throws Exception {
 		Path xslFile = getResourceInsideSchema("formatter/" + profile + "/view.xsl");
 		Path xmlFile = getResource(mdNameRoot + "-19115-3.che.xml");
